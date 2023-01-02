35d05a9b490e0a2625a92d000745d06975598666
dd <dimashechka0213@gmail.com>
2023-01-02 19:46:08 +0200

    fix deploy

--- src/App.js
+++ src/App.js
@@ -20,9 +20,9 @@ export const router = createHashRouter(
       </Route>
 
       <Route path="/" element={<UserLayout />}>
-        <Route path="/wantPlay" element={<WantPlayScreen />} />
-        <Route path="/playingNow" element={<PlayingNowScreen />} />
-        <Route path="/finishedGames" element={<FinishedGames />} />
+        <Route path="/filtermygames/wantPlay" element={<WantPlayScreen />} />
+        <Route path="/filtermygames/playingNow" element={<PlayingNowScreen />} />
+        <Route path="/filtermygames/finishedGames" element={<FinishedGames />} />
         <Route path="/" element={<HomeScreen />} />
       </Route>
     </>
--- src/App/loginScreen/LoginScreen.js
+++ src/App/loginScreen/LoginScreen.js
@@ -4,20 +4,20 @@ import "../../assets/steamShop.png";
 export default function LoginScreen() {
   return (
     <div
-      className="z-0 bg-main-img bg-fixed bg-no-repeat bg-center bg-cover grid content-center px-5"
+      class="z-0 bg-main-img bg-fixed bg-no-repeat bg-center bg-cover grid content-center px-5"
       style={{
         width: "100vw",
         height: "100vh",
       }}
     >
-      <div className="z-50 bg-[#f8fafc] px-5 py-5 rounded-md mx-auto ">
-        <h1 className="text-2xl">Log in to continue</h1>
-        <p className="text-sm font-thin">
+      <div class="z-50 bg-[#f8fafc] px-5 py-5 rounded-md mx-auto ">
+        <h1 class="text-2xl">Log in to continue</h1>
+        <p class="text-sm font-thin">
           Log in by using one of the specified methods
         </p>
-        <div className="flex mt-4">
+        <div class="flex mt-4">
           <img
-            className="mr-5"
+            class="mr-5"
             src="https://cdn-icons-png.flaticon.com/512/3/3782.png"
             alt="steam"
             width="60"
