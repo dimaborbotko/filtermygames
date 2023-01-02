179e76c7ff3bf588b7d55e526dfb7808261a58df
dd <dimashechka0213@gmail.com>
2023-01-02 19:11:11 +0200

    fix deploy

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
 } from "react-router-dom";
-import UserLayout from "./components/userLayout/UserLayout";
+import UserLayout from "./components/layouts/UserLayout";
 import HomeScreen from "./App/homeScreen/HomeScreen";
 import WantPlayScreen from "./App/wantToPlayScreen/WantPlayScreen";
 import PlayingNowScreen from "./App/payingNowScreen/PlayingNowScreen";
 import FinishedGames from "./App/finishedGamesScreen/FinishedGames";
-import AuthLayout from "./components/authLayout/AuthLayout";
+import AuthLayout from "./components/layouts/AuthLayout";
 import LoginScreen from "./App/loginScreen/LoginScreen";
 
 export const router = createBrowserRouter(
@@ -19,11 +19,11 @@ export const router = createBrowserRouter(
         <Route path="/login" element={<LoginScreen />} />
       </Route>
 
-      <Route path="/" element={<UserLayout />}>
-        <Route path="/wantPlay" element={<WantPlayScreen />} />
+      <Route path="/filtermygames" element={<UserLayout />}>
+        <Route path="/filtermygames/wantPlay" element={<WantPlayScreen />} />
         <Route path="/playingNow" element={<PlayingNowScreen />} />
         <Route path="/finishedGames" element={<FinishedGames />} />
-        <Route path="/" element={<HomeScreen />} />
+        <Route path="/filtermygames" element={<HomeScreen />} />
       </Route>
     </>
   )
